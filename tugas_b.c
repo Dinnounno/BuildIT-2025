93288115edbb3f32d48a70416aa8a04fd06b4c4f
Sikhapryn <madesikhahitapriyanti@gmail.com>
2025-10-08 09:31:23 +0800

    Update tugas_b.c

--- tugas_b.c
+++ tugas_b.c
@@ -5,7 +5,6 @@ int main() {
     scanf("%d", &n);
 
     for (int i = 1; i < n; i++) {
-        // Spasi di kiri: cukup (n - 1 - i)
         for (int j = 1; j <= n - 1 - i; j++) {
             printf(" ");
         }
