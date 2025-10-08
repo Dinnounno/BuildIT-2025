b32efac22563d97f7022145d924ba2b8af4ab57f
Sikhapryn <madesikhahitapriyanti@gmail.com>
2025-10-08 09:32:21 +0800

    Update tugas_b.c

--- tugas_b.c
+++ tugas_b.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
 int main() {
-    int n;
-    scanf("%d", &n);
+int bin;
+    scanf("%d", &bin);
 
-    for (int i = 1; i < n; i++) {
-        for (int j = 1; j <= n - 1 - i; j++) {
+     for (int i = 1; i < bin; i++) {
+        for (int j = i; j < bin - 1; j++) {
             printf(" ");
         }
-        for (int k = 1; k <= 2 * i - 1; k++) {
+        for (int k = 1; k <= (2 * i - 1); k++) {
             printf("*");
         }
-
         printf("\n");
     }
-
-    for (int j = 1; j <= n - 2; j++) {
+    for (int j = 1; j < bin - 1; j++) {
         printf(" ");
     }
     printf("*\n");
-return 0;
+    return 0;
 }
